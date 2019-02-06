8c9ef30d199123c38f35a6ed20a1b3a8a8e4c291
yujiangliu <yujiang.liu@wustl.edu>
2019-02-05 23:44:20 -0600

    feat: login send data
    Change-Id: Ibf64725ad1b8bda9c07c2f5ccaa52d4b92a957ea

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Register } from "./Register";
+import { Login } from "./Login";
 
 export class Main extends React.Component {
     // under the topbar
     render() {
         return (
             <div className='main'>
-                <Register/>
+                <Login/>
             </div>
         );
     }
