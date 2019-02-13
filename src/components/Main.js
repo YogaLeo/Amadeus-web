d3f640a14195b95e5bbbcfa6c97f05ceb06bdbfb
yujiangliu <yujiang.liu@wustl.edu>
2019-02-12 20:55:58 -0600

    feat: finish relation between login logout home
    Change-Id: I3d48633ff8486de7636a0bd60cb3781e59adcbf3

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {Home} from './Home';
 import { Login } from "./Login";
 import { Register } from "./Register";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 export class Main extends React.Component {
     // under the topbar
     getLogin = () =>{
         //if user have login , we should render to home, otherwise render to login
-        return this.props.isLoggedIn ? <Home/> : <Login handleSuccessfulLogin = {this.props.handleSuccessfulLogin}/>
+        return this.props.isLoggedIn ? <Redirect to='/home'/> : <Login handleSuccessfulLogin = {this.props.handleSuccessfulLogin}/>
+    }
+
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to='/login'/>;
     }
 
     render() {
@@ -18,7 +22,7 @@ export class Main extends React.Component {
                     <Route exact path="/" render={this.getLogin}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route path="/home" component={Home}/>
+                    <Route path="/home" render={this.getHome}/>
                     <Route render={this.getLogin}/>
                 </Switch>
             </div>
