0913bb96716e8856752e6bea53f3583b60ba7c07
yujiangliu <yujiang.liu@wustl.edu>
2019-02-12 18:10:03 -0600

    feat: import react browser router
    Change-Id: I4643804ea969867d68faeea410ae5ccede270d1e

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Register } from "./Register";
 import { Login } from "./Login";
+import { Register } from "./Register";
+import { Switch, Route } from 'react-router-dom';
 
 export class Main extends React.Component {
     // under the topbar
     render() {
         return (
             <div className='main'>
-                <Login/>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route component={Login}/>
+                </Switch>
             </div>
         );
     }
